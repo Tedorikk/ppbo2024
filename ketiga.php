ce598d4feb6b2d63f7c7d0339f851f85a45f4818
Tedrick <tedrikex@gmail.com>
2024-08-30 15:09:02 +0700

    Praktikum 4B - Membuat Atribut

--- ketiga.php
+++ ketiga.php
@@ -3,15 +3,16 @@
 class Lingkaran
 {
     const PHI = 3.14;
+    public $jari_jari;
 
 
-    public function luas($jari_jari) : float {
-        return self::PHI*$jari_jari*$jari_jari;
+    public function luas() : float {
+        return self::PHI*$this->jari_jari*$this->jari_jari;
     }
 
 
-    public function keliling($jari_jari) : float {
-        return 2*self::PHI*$jari_jari;
+    public function keliling() : float {
+        return 2*self::PHI*$this->jari_jari;
     }
 }
 
@@ -19,8 +20,9 @@ class Lingkaran
 class Bola
 {
     const PHI = 3.14;
-    public function volume($jari_jari) : float {
-        return (4/3)*self::PHI*pow($jari_jari,3);
+    public $jari_jari;
+    public function volume() : float {
+        return (4/3)*self::PHI*pow($this->jari_jari,3);
     }
 }
 
@@ -28,8 +30,10 @@ class Bola
 class Tabung
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+    public function volume() : float {
+        return self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
@@ -37,12 +41,30 @@ class Tabung
 class Kerucut
 {
     const PHI = 3.14;
-    public function volume($jari_jari,$tinggi) : float {
-        return (1/3)*self::PHI*pow($jari_jari,2)*$tinggi;
+    public $jari_jari;
+    public $tinggi;
+    public function volume() : float {
+        return (1/3)*self::PHI*pow($this->jari_jari,2)*$this->tinggi;
     }
 }
 
 $nasiTumpeng = new Kerucut();
-$volumeNasiTumpeng = $nasiTumpeng -> volume(4,10);
+$nasiTumpeng->jari_jari = 4;
+$nasiTumpeng->tinggi = 10;
+$volumeNasiTumpeng = $nasiTumpeng -> volume();
 
 echo "volume nasi tumpeng yaitu {$volumeNasiTumpeng}";
+
+$bola = new Bola();
+$bola->jari_jari = 7;
+$volumeBola = $bola->volume();
+
+echo "\nVolume bola adalah {$volumeBola}";
+
+// Implementasi Tabung
+$tabung = new Tabung();
+$tabung->jari_jari = 5;
+$tabung->tinggi = 15;
+$volumeTabung = $tabung->volume();
+
+echo "\nVolume tabung adalah {$volumeTabung}";
